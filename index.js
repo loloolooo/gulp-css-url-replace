d60cdb46efcdb2135d9e67eca564550451102ba8
xianghongai <xianghongai@gmail.com>
2019-03-04 15:41:59 +0800

    fix: upgrade getFileExtension function

--- index.js
+++ index.js
@@ -3,45 +3,53 @@ const reworkUrl = require('rework-plugin-url');
 const through = require('through2');
 
 module.exports = function (param) {
-    param = param || {};
-    const img = param.img || '/assets/img/';
-    const font = param.font || '/assets/font/';
-    const FONT = ["eot", "ttf", "woff", "woff2", "font"];
+  param = param || {};
+  const img = param.img || '/assets/img/';
+  const font = param.font || '/assets/font/';
+  const FONT = ["eot", "ttf", "woff", "woff2", "font"];
 
-    function fileExtension(file) {
-        return file.slice((file.lastIndexOf('.') - 1 >>> 0) + 2).toLowerCase();;
+  /**
+   * get file extensions
+   * https://stackoverflow.com/a/38285146/5819157
+   * @param  {string} url
+   * @returns {string} - extension
+   */
+  function getFileExtension(file) {
+    var regexp = /\.([0-9a-z]+)(?:[\?#]|$)/i;
+    var extension = file.match(regexp);
+    return extension && extension[1];
+  }
+
+  function isInArray(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+      if (value.indexOf(arr[i]) > -1) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    function isInArray(arr, value) {
-        for (let i = 0; i < arr.length; i++) {
-            if (value.indexOf(arr[i]) > -1) {
-                return true;
-            }
+  function replaceUrl(css) {
+    let prefix = "";
+    return rework(css)
+      .use(reworkUrl(function (url) {
+        if (url.indexOf('data:') === 0) {
+          return url;
+        } else if (/font/.test(url) && /svg/.test(getFileExtension(url)) || isInArray(FONT, getFileExtension(url))) {
+          prefix = font;
+        } else {
+          prefix = img;
         }
-        return false;
-    }
+        return prefix + url.substring(url.lastIndexOf("/") + 1);
+      }))
+      .toString();
+  };
 
-    function replaceUrl(css) {
-        let prefix = "";
-        return rework(css)
-            .use(reworkUrl(function (url) {
-                if (url.indexOf('data:') === 0) {
-                    return url;
-                } else if (/font/.test(url) && /svg/.test(fileExtension(url)) || isInArray(FONT, fileExtension(url))) {
-                    prefix = font;
-                } else {
-                    prefix = img;
-                }
-                return prefix + url.substring(url.lastIndexOf("/") + 1);
-            }))
-            .toString();
-    };
+  return through.obj(function (file, enc, cb) {
+    let css = replaceUrl(file.contents.toString());
+    file.contents = new Buffer(css);
 
-    return through.obj(function (file, enc, cb) {
-        let css = replaceUrl(file.contents.toString());
-        file.contents = new Buffer(css);
-
-        this.push(file);
-        cb();
-    });
+    this.push(file);
+    cb();
+  });
 };
